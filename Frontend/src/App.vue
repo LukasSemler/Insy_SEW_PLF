6ef118fc50184567af6c4062b12d481861461a22
LukasSemler <semler.l04@htlwienwest.at>
2022-05-01 16:40:47 +0200

    Router transitions

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -17,7 +17,23 @@ onMounted(() => {
 <template>
   <CompNavBar></CompNavBar>
   <div>
-    <RouterView></RouterView>
+    <RouterView v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component"></component>
+      </transition>
+    </RouterView>
   </div>
   <CompFooter></CompFooter>
 </template>
+
+<style>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-out;
+}
+</style>
